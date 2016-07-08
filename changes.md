33151fcac01fc64669f491fd52a86581197e77c9
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-07-08 14:03:02 +0200

    v0.6.1-alpha1: properly versioned for deployment

--- changes.md
+++ changes.md
@@ -1,3 +1,10 @@
+## v0.6.1-alpha1 - July 8th, 2016
+
+```clojure
+[matthiasn/systems-toolbox-redis "0.6.1-alpha1"]
+```
+
+
 ## v0.6.1-SNAPSHOT - June 30th, 2016
 
 ```clojure
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matthiasn/systems-toolbox-redis "0.6.1-SNAPSHOT"
+(defproject matthiasn/systems-toolbox-redis "0.6.1-alpha1"
   :description "Redis communication components for systems-toolbox"
   :url "https://github.com/matthiasn/systems-toolbox-redis"
   :license {:name "Eclipse Public License"
