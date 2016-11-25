b8e58e53e49119b3ae9e2ba4b17e16b8d3a1f74d
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-11-25 19:27:22 +0100

    v0.6.2: deps cleanup

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matthiasn/systems-toolbox-redis "0.6.1"
+(defproject matthiasn/systems-toolbox-redis "0.6.2"
   :description "Redis communication components for systems-toolbox"
   :url "https://github.com/matthiasn/systems-toolbox-redis"
   :license {:name "Eclipse Public License"
@@ -7,10 +7,10 @@
   :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/tools.logging "0.3.1"]
-                 [matthiasn/systems-toolbox "0.6.1"]
                  [com.taoensso/carmine "2.15.0"]]
 
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                                  [matthiasn/systems-toolbox "0.6.2"]]}}
 
   :plugins [[lein-codox "0.10.2"]
             [lein-cloverage "1.0.9"]])
